14c3256db26bd43488b1288ea1feb762a89a2a0a
RDnante <nanterakoto15@gmail.com>
2023-01-12 15:01:54 +0300

    base + salaire_poste

--- code_garage/Base.sql
+++ code_garage/Base.sql
@@ -5,6 +5,18 @@ create table poste (
     nom_poste varchar(50)
 );
 
+create table niveau_etude (
+    idniveau serial primary key,
+    niveau varchar(100)
+);
+
+create table salaire_poste(
+    idsalaire_poste serial primary key,
+    idposte int,
+    salaire double precision,
+    foreign key (idposte) references poste(idposte) 
+);
+
 create table Employe (
     idemploye serial primary key,
     nom varchar(100),
@@ -16,10 +28,7 @@ create table Employe (
     foreign key (idniveau) references niveau_etude(idniveau)
 );
 
-create table niveau_etude (
-    idniveau serial primary key,
-    niveau varchar(100)
-);
+
 
 insert into poste values (default,'Controleur de Vehicule');
 insert into poste values (default,'Peintre en carrosserie'); 
@@ -30,4 +39,9 @@ insert into niveau_etude values (default,'CAP');
 insert into niveau_etude values (default,'BACC+2');
 insert into niveau_etude values (default,'Ingeniurie');
 insert into niveau_etude values (default,'BACC+5');
+
+insert into salaire_poste values (default,1,40000);
+insert into salaire_poste values (default,2,20000);
+insert into salaire_poste values (default,3,30000);
+insert into salaire_poste values (default,4,25000);
 
