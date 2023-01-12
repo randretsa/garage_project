df16982e57738b364b7a22ce300175aa81ea3371
Rsm-mah <mahrasamisoa@gmail.com>
2023-01-12 22:11:52 +0100

    creation class
    Class : Service,ServicePoste,SalairePoste

---
+++ code_garage/src/java/metier/ServicePoste.java
@@ -0,0 +1,27 @@
+package metier;
+
+public class ServicePoste {
+    SalairePoste poste;
+    double dure;
+
+    public ServicePoste() {
+
+    }
+
+    public ServicePoste(SalairePoste poste,double dure) {
+
+    }
+
+    public SalairePoste getPoste() {
+        return poste;
+    }
+    public void setPoste(SalairePoste poste) {
+        this.poste = poste;
+    }
+    public double getDure() {
+        return dure;
+    }
+    public void setDure(double dure) {
+        this.dure = dure;
+    }
+}
